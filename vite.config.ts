9fa90e002fc48cef8e4675cd098052aea95d00e7
Neemias Manso <neemias.manso@jcgestaoderiscos.com.br>
2026-03-11 15:18:14 -0300

    feat: testing environments

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
 
 export default defineConfig({
   plugins: [react(), svgr()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['./src/setupTests.ts'],
+    css: true,
+    include: ['src/__tests__/**/*.test.{ts,tsx}'],
+  },
 });
